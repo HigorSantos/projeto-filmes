f62ab5887e44d8a4276805054cc3a85039459acc
Higor Santos <higor.santos92@yahoo.com.br>
2020-06-20 21:22:04 -0300

    Updates

---
+++ precache-manifest.6a05d664eba9860fdf4e4f965d71683d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b8da533d4f2bab795d8094fe1ec7e4d",
+    "url": "/projeto-filmes/index.html"
+  },
+  {
+    "revision": "e8b592676998fe2c8ba7",
+    "url": "/projeto-filmes/static/css/main.6a1776c2.chunk.css"
+  },
+  {
+    "revision": "41916c05e3d58bcd3b85",
+    "url": "/projeto-filmes/static/js/2.4b9e7a49.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/projeto-filmes/static/js/2.4b9e7a49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8b592676998fe2c8ba7",
+    "url": "/projeto-filmes/static/js/main.954d04be.chunk.js"
+  },
+  {
+    "revision": "343a34fdfb716983692b",
+    "url": "/projeto-filmes/static/js/runtime-main.59220a35.js"
+  },
+  {
+    "revision": "bb221830280f32dfb6a2c7c3b3fc0d38",
+    "url": "/projeto-filmes/static/media/logo.bb221830.png"
+  },
+  {
+    "revision": "56fd61eec148690af48f876dfd5c8718",
+    "url": "/projeto-filmes/static/media/tmdb-blue_short.56fd61ee.svg"
+  }
+]);
